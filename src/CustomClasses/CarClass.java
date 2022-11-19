96ebe376760af640349a182a40d4dc6b488b9b6c
Murat <muratgunarslan@mail.com>
2022-11-18 22:08:01 -0500

    update17

---
+++ src/CustomClasses/CarClass.java
@@ -0,0 +1,16 @@
+package CustomClasses;
+
+public class CarClass {
+    //Fields
+
+    public String Make;
+    public String Model;
+    public int ModelYear;
+    public String Color;
+
+    private double Cost;
+
+    //Methods
+
+
+}
---
+++ src/Day26_ClassesandConstructors/Topic2_Constructor.java
@@ -0,0 +1,5 @@
+package Day26_ClassesandConstructors;
+
+public class Topic2_Constructor {
+
+}
